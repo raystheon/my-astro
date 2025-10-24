86795fcce7a5ba22c3dbfecadf6fe046339f4f2e
Raymond Anguiano <raymondanguiano83@gmail.com>
2025-10-24 03:22:17 +0000

    Final attempt: Ignoring node_modules and forcing commit of F-Droid preparation changes

---
+++ magi-astro/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.raystheon.myastro',
+  appName: 'magi-astro',
+  webDir: '.'
+};
+
+export default config;
